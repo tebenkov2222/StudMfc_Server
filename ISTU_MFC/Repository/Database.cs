f6f388caf2f4ff2a4dcfc50c7c465127a6a3618a
Hartman <52998392+tebenkov2222@users.noreply.github.com>
2022-11-11 20:54:07 +0400

    add repository

---
+++ ISTU_MFC/Repository/Database.cs
@@ -0,0 +1,19 @@
+namespace Repository
+{
+    public class Database
+    {
+        public Database()
+        {
+        }
+
+        public void Connect(DatabaseConnectData databaseConnectData)
+        {
+            
+        }
+
+        public void Disconnect()
+        {
+            
+        }
+    }
+}
---
+++ ISTU_MFC/Repository/DatabaseConnectData.cs
@@ -0,0 +1,26 @@
+namespace Repository
+{
+    public class DatabaseConnectData
+    {
+        public string Server;
+        public string Port;
+        public string UserId;
+        public string Password;
+        public string Database;
+        public int Timeout;
+        public int CommandTimeout;
+        public int KeepAlive;
+
+        public static DatabaseConnectData NiardanDefaultData => new DatabaseConnectData()
+        {
+            Server = "niardan.ru",
+            Port = "54334",
+            UserId = "tebenkov2222",
+            Password = "TP_Linkenew",
+            Database = "tebenkov2222",
+            Timeout = 300,
+            CommandTimeout = 300,
+            KeepAlive = 300
+        };
+    }
+}
