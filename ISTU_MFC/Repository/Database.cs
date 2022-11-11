1bd487137ea13b71b718c750c4c19c88453810f6
Mario2k <103027923+Mario2k@users.noreply.github.com>
2022-11-11 21:09:24 +0400

    test
    протестил

--- ISTU_MFC/Repository/Database.cs
+++ ISTU_MFC/Repository/Database.cs
@@ -1,14 +1,16 @@
 namespace Repository
 {
+    
     public class Database
     {
         public Database()
         {
+            
         }
 
         public void Connect(DatabaseConnectData databaseConnectData)
         {
-            
+            var db = new NpgsqlConnection("Host=localhost;Port=5432;Database=test;Username=postgres;Password=1");
         }
 
         public void Disconnect()
