0869465046fb490bc307e3c04d4a78bf3cc723f7
Hartman <52998392+tebenkov2222@users.noreply.github.com>
2022-12-13 19:05:14 +0400

    preview pdf in doc generation

---
+++ ISTU_MFC/ISTU_MFC/ViewModels/FormFieldViewModel.cs
@@ -0,0 +1,9 @@
+namespace ISTU_MFC.ViewModels
+{
+    public class FormFieldViewModel
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string SelectedType { get; set; }
+    }
+}
