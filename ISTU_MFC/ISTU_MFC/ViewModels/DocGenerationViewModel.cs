771b9defc989c06b7e38736bd9820b6401df670d
Hartman <52998392+tebenkov2222@users.noreply.github.com>
2022-12-14 22:43:57 +0400

    fix view

--- ISTU_MFC/ISTU_MFC/ViewModels/DocGenerationViewModel.cs
+++ ISTU_MFC/ISTU_MFC/ViewModels/DocGenerationViewModel.cs
@@ -6,11 +6,41 @@ namespace ISTU_MFC.ViewModels
     public class DocGenerationViewModel
     {
         public string PathToPreviewDoc { get; set; }
-        public string RelativePathToPreviewDoc { get; set; }
+        public string PathToOutputDoc { get; set; }
         public string PathToFormDoc { get; set; }
         public bool IsHasDoc { get; set; }
         public bool IsNotHasDoc => !IsHasDoc;
         public List<FormFieldViewModel> FormFields { get; set; }
-        public SelectList SelectList { get; set; }
+
+        public static List<SelectListItem> DefaultSelectList()
+        {
+            var groupStudent = new SelectListGroup() {Name = "Студент"};
+            return new List<SelectListItem>()
+            {
+                new SelectListItem()
+                {
+                    Text = "Пустое поле",
+                    Value = "FieldDefault"
+                },
+                new SelectListItem()
+                {
+                    Text = "Имя студента",
+                    Value = "NameStudentField",
+                    Group = groupStudent
+                },
+                new SelectListItem()
+                {
+                    Text = "Фамилия студента",
+                    Value = "SurnameStudentField",
+                    Group = groupStudent
+                },
+                new SelectListItem()
+                {
+                    Text = "Группа студента",
+                    Value = "GroupStudentField",
+                    Group = groupStudent,
+                },
+            };
+        }
     }
 }
